882bd742f9c2d58f92e57488ea97942b0a85aff5
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-12-06 14:56:35 -0500

    fix: async request APIs v15

--- src/app/dashboard/summaries/[videoId]/page.tsx
+++ src/app/dashboard/summaries/[videoId]/page.tsx
@@ -1,15 +1,10 @@
 import { SummaryCardForm } from "@/components/custom/forms/summary-card-form"
 import { getSummaryById } from "@/services/strapi"
 
-interface ParamsProps {
-    params: {
-        videoId: string
-    }
-}
+type Params = Promise<{ videoId: string }>
 
-export default async function SummaryCardRoute(props: Readonly<ParamsProps>) {
-    const params = await props?.params
-    const { videoId } = params
+export default async function SummaryCardRoute(props: { params: Params }) {
+    const { videoId } = await props.params
     const data = await getSummaryById(videoId)
     return <SummaryCardForm item={data.data} />
 }
